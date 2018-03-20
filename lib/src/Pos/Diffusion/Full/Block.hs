dde62bc2bba7bb1fa13af7877cdffea14f4139b2
Alexander Vieth <aovieth@gmail.com>
2018-03-20 13:14:01 -0400

    [CSL-2331] EnqueueMsg shows the TVar PackStatus (#2679)
    * [CSL-2331] EnqueueMsg shows the TVar PackStatus
    
    * pkgs/default.nix
    
    * [CSL-2331] remove MonadThrow constraint
    
    * [CSL-2331] fix minor comment typo

--- lib/src/Pos/Diffusion/Full/Block.hs
+++ lib/src/Pos/Diffusion/Full/Block.hs
@@ -13,6 +13,8 @@ module Pos.Diffusion.Full.Block
 
 import           Universum
 
+import qualified Control.Concurrent.STM as Conc
+import           Control.Exception (throwIO)
 import           Control.Exception.Safe (Exception (..))
 import           Control.Lens (to)
 import           Control.Monad.Except (ExceptT, runExceptT, throwError)
@@ -37,7 +39,7 @@ import           Pos.Communication.Protocol (Conversation (..), ConversationActi
                                              EnqueueMsg, ListenerSpec, MkListeners (..),
                                              MsgType (..), NodeId, Origin (..), OutSpecs,
                                              constantListeners, waitForConversations,
-                                             recvLimited)
+                                             waitForDequeues, recvLimited)
 import           Pos.Core (BlockVersionData, HeaderHash, ProtocolConstants (..), bvdSlotDuration,
                            headerHash, prevBlockL)
 import           Pos.Core.Block (Block, BlockHeader (..), MainBlockHeader, blockHeader)
@@ -86,17 +88,17 @@ instance Exception BlockNetLogicException where
 -- | Expects sending message to exactly one node. Receives result or
 -- fails if no result was obtained (no nodes available, timeout, etc).
 enqueueMsgSingle ::
-       ( MonadThrow m )
-    => (t2 -> (t1 -> t -> NonEmpty x) -> m (Map NodeId (m b)))
+       ( MonadIO m )
+    => (t2 -> (t1 -> t -> NonEmpty x) -> m (Map NodeId (Conc.TVar (OQ.PacketStatus b))))
     -> t2
     -> x
     -> m b
 enqueueMsgSingle enqueue msg conv = do
-    results <- enqueue msg (\_ _ -> one conv) >>= waitForConversations
+    results <- enqueue msg (\_ _ -> one conv) >>= waitForConversations . waitForDequeues
     case toList results of
-        [] ->      throwM $ DialogUnexpected $
+        [] ->      liftIO $ throwIO $ DialogUnexpected $
             "enqueueMsgSingle: contacted no peers"
-        (_:_:_) -> throwM $ DialogUnexpected $
+        (_:_:_) -> liftIO $ throwIO $ DialogUnexpected $
             "enqueueMsgSingle: contacted more than one peers, probably internal error"
         [x] -> pure x
 
@@ -290,7 +292,7 @@ requestTip
     -> (BlockHeader -> NodeId -> d t)
     -> d (Map NodeId (d t))
 requestTip logic enqueue k =
-    enqueue (MsgRequestBlockHeaders Nothing) $ \nodeId _ -> pure . Conversation $
+    fmap waitForDequeues $ enqueue (MsgRequestBlockHeaders Nothing) $ \nodeId _ -> pure . Conversation $
         \(conv :: ConversationActions MsgGetHeaders MsgHeaders m) -> do
             logDebug "Requesting tip..."
             bvd <- getAdoptedBVData logic
@@ -320,7 +322,7 @@ announceBlockHeader
     -> d (Map NodeId (d ()))
 announceBlockHeader logic protocolConstants recoveryHeadersMessage enqueue header =  do
     logDebug $ sformat ("Announcing header to others:\n"%build) header
-    enqueue (MsgAnnounceBlockHeader OriginSender) (\addr _ -> announceBlockDo addr)
+    waitForDequeues <$> enqueue (MsgAnnounceBlockHeader OriginSender) (\addr _ -> announceBlockDo addr)
   where
     announceBlockDo nodeId = pure $ Conversation $ \cA -> do
         -- TODO figure out what this security stuff is doing and judge whether
@@ -481,8 +483,8 @@ handleGetBlocks logic recoveryHeadersMessage oq = listenerConv oq $ \__ourVerInf
             Right hashes -> do
                 logDebug $ sformat
                     ("handleGetBlocks: started sending "%int%
-                     " blocks to "%build%" one-by-one: "%listJson)
-                    (length hashes) nodeId hashes
+                     " blocks to "%build%" one-by-one")
+                    (length hashes) nodeId 
                 for_ hashes $ \hHash ->
                     getBlock logic hHash >>= \case
                         Just b -> send conv $ MsgBlock b
